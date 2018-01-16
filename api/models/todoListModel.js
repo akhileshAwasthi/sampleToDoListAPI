b8baae28d04a1b7031d4da300ff5a75e95fb770d
Akhilesh Awasthi <akhileshawasthi.hbti@gmail.com>
2018-01-16 10:46:04 +0530

    Update todoListModel.js
    Formatted and code comments added.

--- api/models/todoListModel.js
+++ api/models/todoListModel.js
@@ -1,23 +1,28 @@
 
 'use strict';
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-
+// Schema of task to be dedfined for MongoDB
 var TaskSchema = new Schema({
+  
   name: {
     type: String,
     required: 'Kindly enter the name of the task'
   },
+  
   Created_date: {
     type: Date,
     default: Date.now
   },
+  
   status: {
     type: [{
       type: String,
       enum: ['pending', 'ongoing', 'completed']
     }],
+    
     default: ['pending']
   }
 });

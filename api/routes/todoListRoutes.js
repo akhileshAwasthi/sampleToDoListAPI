00486999ca0fcfe0a82cd8b3c43c914740c2228a
Akhilesh Awasthi <akhileshawasthi.hbti@gmail.com>
2018-01-16 10:47:37 +0530

    Update todoListRoutes.js
    Formatted the code.

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -1,8 +1,11 @@
+
 'use strict';
+
 module.exports = function(app) {
-  var todoList = require('../controllers/todoListController');
+var todoList = require('../controllers/todoListController');
 
   // todoList Routes
+  
   app.route('/tasks')
     .get(todoList.list_all_tasks)
     .post(todoList.create_a_task);
@@ -12,4 +15,5 @@ module.exports = function(app) {
     .get(todoList.read_a_task)
     .put(todoList.update_a_task)
     .delete(todoList.delete_a_task);
+  
 };

2073a998e868dac2d702fa8c430e8961e129cb77
Akhilesh Awasthi <akhileshawasthi.hbti@gmail.com>
2018-01-16 10:43:16 +0530

    Update todoListController.js
    Foramatted the code. Comments added.

--- api/controllers/todoListController.js
+++ api/controllers/todoListController.js
@@ -1,9 +1,10 @@
+
 'use strict';
 
-
 var mongoose = require('mongoose'),
-  Task = mongoose.model('Tasks');
+Task = mongoose.model('Tasks');
 
+//to list all the tasks added by user
 exports.list_all_tasks = function(req, res) {
   Task.find({}, function(err, task) {
     if (err)
@@ -14,38 +15,43 @@ exports.list_all_tasks = function(req, res) {
 
 
 
-
+// to create a new task
 exports.create_a_task = function(req, res) {
+ 
   var new_task = new Task(req.body);
   new_task.save(function(err, task) {
     if (err)
       res.send(err);
     res.json(task);
   });
+  
 };
 
-
+// reading a task added earlier
 exports.read_a_task = function(req, res) {
+  
   Task.findById(req.params.taskId, function(err, task) {
     if (err)
       res.send(err);
     res.json(task);
   });
+  
 };
 
-
+// Updating a previously added task
 exports.update_a_task = function(req, res) {
+  
   Task.findOneAndUpdate({_id: req.params.taskId}, req.body, {new: true}, function(err, task) {
     if (err)
       res.send(err);
     res.json(task);
   });
+  
 };
 
-
+//Deleting a task
 exports.delete_a_task = function(req, res) {
-
-
+  
   Task.remove({
     _id: req.params.taskId
   }, function(err, task) {
@@ -53,6 +59,7 @@ exports.delete_a_task = function(req, res) {
       res.send(err);
     res.json({ message: 'Task successfully deleted' });
   });
+  
 };
 
 
